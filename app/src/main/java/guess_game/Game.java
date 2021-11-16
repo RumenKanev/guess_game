63aebd90060a59e0b9cf306e81a8b80d08ae18b7
RumenKanev <rkynev@gmail.com>
2021-11-16 19:29:29 +0000

    chapter five complete

--- app/src/main/java/guess_game/Game.java
+++ app/src/main/java/guess_game/Game.java
@@ -1,7 +1,29 @@
 package guess_game;
 
+
 public class Game {
-    public String getWordToGuess() {
-        return "B_____";
+    String words;
+    Game(String word){
+       words = word;
+        System.out.println(words);
     }
+
+
+
+    public String getWordToGuess() {
+        StringBuilder builder = new StringBuilder(words.length());
+
+        for (int i = 0; i < words.length(); i++) {
+
+            if (i == 0) {
+                builder.append(words.charAt(0));
+            } else {
+                builder.append("_");
+            }
+        }
+
+        return builder.toString();
+    }
+
+
 }
